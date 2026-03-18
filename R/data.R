75b63e34943e384d02de0d4a240992c610be784c
Yiyi Cheng <88147881+YiyiChen0328@users.noreply.github.com>
2026-03-18 16:51:13 +0000

    Adjust for CRAN

--- R/data.R
+++ R/data.R
@@ -2,10 +2,12 @@
 #'
 #' A dataset containing 200 simulated cells(100 per group) for
 #' demonstrating CellDEEP functions.
+#' Can be found at \doi{10.5281/zenodo.18863779}
 #'
 #' @docType data
 #' @source simulated data with muscat package
 #' @usage data(sim)
 #' @format A Seurat object
 #' @name sim
+#'
 NULL
